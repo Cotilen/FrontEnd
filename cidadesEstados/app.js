59060578b9f4cc80cf6575754a4448f8863e5e9e
Cleiton <22282081@CFP127ALUNO.LOCAL>
2023-03-24 09:50:04 -0300

    Completo

--- cidadesEstados/app.js
+++ cidadesEstados/app.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const mapa = document.querySelector('#map')
+var lista = []
+var arrayCidades = []
 
 
 const getEstado = async function({ target }) {
@@ -21,29 +23,88 @@ const getEstado = async function({ target }) {
         }
 
     }
+
+    const getRegiao = async function(sigla) {
+
+        let url = `http://localhost:8080/v1/senai/estado/sigla/${sigla}`
+
+        let response = await fetch(url)
+        let data = await response.json()
+
+        return {
+            regiao: data.regiao
+        }
+
+    }
+
+
     const estadoClicado = await getCidades(estado)
-    console.log(estadoClicado);
+    const regiaoClicado = await getRegiao(estado)
+
+
+    // console.log(regiaoClicado);
+    // console.log(estadoClicado);
 
     const getCard = function(siglaEstado) {
 
-            const container = document.getElementById('container__text')
 
-            const div = document.createElement('div')
-            div.classList.add('text-container')
+        const container = document.getElementById('container__text')
 
-            const sigla = document.createElement('div')
-            sigla.classList.add('text-container__sigla')
-            sigla.textContent = estadoClicado.uf
+        const card = document.createElement('div')
+        card.classList.add('text-container')
+
+        const estado = document.createElement('div')
+        estado.classList.add('text-container-estado')
+
+        const sigla = document.createElement('div')
+        sigla.classList.add('text-container__sigla')
+        sigla.textContent = estadoClicado.uf
+
+        const descricao = document.createElement('div')
+        descricao.classList.add('text-container__descricao')
+
+        const capital = document.createElement('div')
+        capital.classList.add('text-container__descricao__capital')
+        capital.textContent = 'Capital: ' + estadoClicado.descricao
+
+        const regiao = document.createElement('div')
+        regiao.classList.add('text-container__descricao__regiao')
+        regiao.textContent = 'Região: ' + regiaoClicado.regiao
+
+        const seila = document.createElement('div')
+        seila.classList.add('nem_sei')
+
+        const cidadeText = document.createElement('div')
+        cidadeText.classList.add('text-cidade')
+        cidadeText.textContent = 'Cidades: '
+
+        lista.splice([0])
+        lista.push(estadoClicado.cidades)
 
 
-            div.append(sigla)
-            container.replaceChildren(div)
+        const listaCidades = document.createElement('div')
+        listaCidades.classList.add('text-container__cidades')
 
 
+        lista[0].forEach(cidades => {
 
-        }
-        // console.log(estadoClicado.uf);
+            const cidadesMapa = document.createElement('p')
+            cidadesMapa.textContent = cidades
 
+            listaCidades.append(cidadesMapa)
+
+        });
+
+        // console.log(lista);
+
+
+        seila.append(listaCidades)
+        descricao.append(capital, regiao)
+        estado.append(sigla, descricao)
+        card.append(estado, cidadeText, seila)
+
+        container.replaceChildren(card)
+    }
     getCard(estadoClicado.uf)
 
 }
